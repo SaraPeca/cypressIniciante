22fd02fb24f5e2dc1cd9bcc83e72d09b8d439e8e
Sara Peca <sara.sistemas@gmail.com>
2025-03-24 16:56:19 -0300

    adicionando comentário no arquivo config

--- cypress.config.js
+++ cypress.config.js
@@ -4,7 +4,9 @@ module.exports = defineConfig({
   e2e: {
     baseUrl: 'https://automationpratice.com.br/',
     defaultCommandTimeout: 5000,
-
+    // Definir resolução em que os testes devem ser executados:
+    // viewportHeight = 390,
+    // viewportHeight = 844,
     setupNodeEvents(on, config) {
       // implement node event listeners here
      },
